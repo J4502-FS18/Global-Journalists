442c01970bccaab346ddc7f083bd9b1778190dca
Ayesha Vishnani <av4h9@mail.missouri.edu>
2018-11-14 21:03:51 -0600

    homepage updates

---
+++ menu.js
@@ -0,0 +1,14 @@
+//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(400);
+  });//end slide toggle
+
+//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $(window).resize(function() {
+	   if (  $(window).width() > 700 ) {
+		$('nav ul').removeAttr('style');
+	 }
+	});//end resize
+});//end ready
